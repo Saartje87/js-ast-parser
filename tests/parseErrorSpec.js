68d4cefd62b3349ee298d78823f0a4778ff2d83a
Niek Saarberg <niek@pluxbox.nl>
2014-12-16 09:13:47 +0100

    'Start' of errorSpec

--- tests/parseErrorSpec.js
+++ tests/parseErrorSpec.js
@@ -1,1 +1,25 @@
 // @todo handle parse errors
+describe('ParseError', function () {
+
+	it("should throw Strings", function () {
+
+		expect(function () {
+
+			Tokenize("' ");
+		}).toThrow(Error("Unexpected string end"));
+
+		expect(function () {
+
+			Tokenize(" '");
+		}).toThrow(Error("Unexpected string end"));
+	});
+
+
+	// it("should throw Strings", function () {
+
+	// 	expect(Tokenize("a[0, 1]")).toThrow({
+	// 		type : "String",
+	// 		value : "  "
+	// 	});
+	// });
+});
